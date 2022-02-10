529e38aec5ea8335eb222a4236f6b0ff3f2eeb55
Lamine LAKHDARI <11709059@edu.sorbonne-paris-nord.fr>
2022-02-10 17:13:08 +0100

    oui

--- src/Mwen/app/src/main/java/com/example/mwenweou/MainActivity.kt
+++ src/Mwen/app/src/main/java/com/example/mwenweou/MainActivity.kt
@@ -9,14 +9,19 @@ import android.R.id
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.example.mwenweou.AccRequest
+import com.example.mwenweou.AccResponse
+import com.example.mwenweou.PredictorServiceGrpc
+import io.grpc.ManagedChannel
+import io.grpc.ManagedChannelBuilder
 import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
